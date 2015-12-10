21fdb2249b83cfa397de7341944c874449106805
Christine Du <duyuqingecho@gmail.com>
2015-12-10 13:47:01 -0500

    first run and display prediction

--- data_read.py
+++ data_read.py
@@ -1,8 +1,13 @@
 import scipy.io
+# load data from files
+def data_load():
+	train_data = scipy.io.loadmat("train_data.mat")
+	test_data = scipy.io.loadmat("test_data.mat")
+	valid_data = scipy.io.loadmat("valid_data.mat")
+	return train_data["train_data"], train_data["train_labels"].ravel(), valid_data["valid_data"], \
+	valid_data["valid_labels"].ravel(), test_data["test_data"], test_data["test_label"].ravel()
+	# print train_data["train_data"].shape
+	# print test_data["test_data"].shape
+	# print valid_data["valid_data"].shape
 
-train_data = scipy.io.loadmat("train_data.mat")
-test_data = scipy.io.loadmat("test_data.mat")
-valid_data = scipy.io.loadmat("valid_data.mat")
-print train_data["train_data"].shape
-print test_data["test_data"].shape
-print valid_data["valid_data"].shape
+
--- train_data.py
+++ train_data.py
@@ -3,8 +3,9 @@ import numpy as np
 from skimage.segmentation import slic
 from skimage.util import img_as_float
 from scipy import misc
-from feature_extraction import *
-from data_preprocessing import *
+from feature import *
+from data_read import *
+from skimage.segmentation import mark_boundaries
 
 from sklearn import svm, metrics
 from sklearn.neighbors import KNeighborsClassifier
@@ -14,12 +15,14 @@ from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 class road_estimation:
 	def __init__(self, model_selection):
-    self._train_data, self._train_targets, self._valid_data, \
-    self._valid_targets, self._test_data, self._test_targets = data_preprocessing()
-    
-    self._model_selection = model_selection
-    self._classifier = None
-	
+		self._train_data, self._train_targets, self._valid_data, \
+		self._valid_targets, self._test_data, self._test_targets = data_load()
+	    
+		self._model_selection = model_selection
+		self._classifier = []
+
+
+
 	def train(self):
 		if self._model_selection == "svm":
 			# selected the svc in svm
@@ -52,15 +55,43 @@ class road_estimation:
 		print("Classification report for classifier %s on valid_data:\n%s\n"
 	      % (self._model_selection, metrics.classification_report(self._valid_targets, prediction_valid)))
 
-	def test(test_data, test_targets):
+	def test(self):
 		# predict on test data
-		prediction_test = self._classifier.predict(self._test_data)
+		prediction_test = self._classifier.predict(self.test_data)
 		# print test result for selected model.
 		print("Classification report for classifier %s on test_data:\n%s\n"
 	      % (self._model_selection, metrics.classification_report(self._test_targets, prediction_test)))
 
 
-	def showPredictionImage(img_original, prediction_label):
+	def showPredictionImage(self):
+		f = Feature()
+		f.loadImage("um_000000.png")
+		f.extractFeatures()
+		fea_matrix = f.getFeaturesVectors()
+
+		predict = self._classifier.predict(fea_matrix)
+		image = np.copy(f.image)
+
+		num_superpixels = np.max(f.superpixel) + 1
+		for i in xrange(0,num_superpixels):
+			indices = np.where(f.superpixel==i)
+			if predict[i] == 1:
+		    	
+				image[indices[0],indices[1],0] = 1
+				image[indices[0],indices[1],1] = 1
+				image[indices[0],indices[1],2] = 0
+		plt.imshow(image)
+		plt.show()
+		# show prediction image with superpixels
+		plt.imshow(mark_boundaries(image, superpixels))
+		plt.show()
+
+if __name__ == '__main__':
+	road_est = road_estimation("svm")
+	road_est.train()
+	road_est.showPredictionImage()
+
+            
 
 
 
